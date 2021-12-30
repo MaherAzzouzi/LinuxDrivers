50940f071f790b9b19465023626884bea57bb4a9
Maher Azzouzi <maherazz04@gmail.com>
2021-12-30 16:00:05 +0100

    rwsem read/write semaphors, the next version of semaphores when writes and reads are locked

--- driver8/module.c
+++ driver8/module.c
@@ -8,6 +8,12 @@
 // To avoid race conditions.
 #include <linux/semaphore.h>
 
+// For read/write semaphores
+// The idea if two threads both want to access a data structure for reading
+// There is no purpose for making one run and the other waiting on the lock
+// They only want to read
+#include <linux/rwsem.h>
+
 static dev_t first;
 static struct class *cls;
 static struct device *device;
@@ -15,16 +21,28 @@ static struct cdev *cdev;
 
 DEFINE_SEMAPHORE(lock);
 
+
+// Lock only `critical sections` that will perform read or write operations.
+// This have speed benefits.
+// downgrade_write() can be used to unlock all other readers to read newly created data.
+// It allows one writer or unlimited number of readers to hold the semaphore.
+// Writers have prio, as soon as a write tries to enter the critical section no readers
+// will be allowed to read until the write is finished.
+// (it will be efficient when the number of writes is small than the number of reads)
+struct rw_semaphore sem;
+
 int i;
 
 // I will be using read to increment i, and write to show i.
 // It should be locked properly so it can not be incremented twice before getting printed.
 static ssize_t mymodule_read(struct file* filp, char __user *buf, size_t count, loff_t *fpos)
 {
+    down_write(&sem);
     // We have the lock we can increment i
     i++;
 
     // Release the lock here.
+    up_write(&sem);
     up(&lock);
     return 0;
 }
@@ -36,7 +54,11 @@ static ssize_t mymodule_write(struct file* filp, const char __user *buf, size_t 
         return -ERESTARTSYS;
     }
 
+    down_read(&sem);
+
     printk(KERN_ALERT "The number is %d\n", i);
+
+    up_read(&sem);
 
     return count;
 }
@@ -82,6 +104,9 @@ static int __init mymodule_init(void)
     }
     cdev_add(cdev, first, 1);
     cdev->ops = &fops;
+
+    init_rwsem(&sem);
+
     printk(KERN_ALERT "Module loaded successfuly!\n");
     return 0;
 }
